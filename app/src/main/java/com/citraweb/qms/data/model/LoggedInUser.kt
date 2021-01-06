59dc251a12ccf8171f8875bd723642b6a437193a
Mursit Affandi <mursit@citra.web.id>
2021-01-06 10:45:14 +0700

    Initial commit

---
+++ app/src/main/java/com/citraweb/qms/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.citraweb.qms.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+        val userId: String,
+        val displayName: String
+)
