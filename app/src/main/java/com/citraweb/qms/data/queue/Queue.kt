1554008384319161a106e99ebd1b999b7394cf1a
Mursit Affandi <mursit@citra.web.id>
2021-01-19 09:37:19 +0700

    repoing queue

--- app/src/main/java/com/citraweb/qms/data/queue/Queue.kt
+++ app/src/main/java/com/citraweb/qms/data/queue/Queue.kt
@@ -1,3 +1,12 @@
 package com.citraweb.qms.data.queue
 
-data class Queue(val id : String, val name: String , val createdBy : String)
+data class Queue(
+        val id : String,
+        val name: String ,
+        val companyId : String,
+        val createdAt : String,
+        val currentQueue : Int,
+        val isOpen : Boolean,
+        val prefix : String,
+        val waiting : Int,
+        )
--- app/src/main/java/com/citraweb/qms/data/queue/QueueRepository.kt
+++ app/src/main/java/com/citraweb/qms/data/queue/QueueRepository.kt
@@ -5,19 +5,6 @@ import com.citraweb.qms.utils.Result
 import com.google.firebase.auth.FirebaseUser
 
 interface QueueRepository {
-    suspend fun registerUserFromAuthWithEmailAndPassword(
-        name: String,
-        email: String,
-        password: String
-    ): Result<FirebaseUser?>
-    suspend fun createUserInFirestore(user: User): Result<Void?>
-    suspend fun loginUserInFirestore(email: String, password: String): Result<FirebaseUser?>
-    fun logoutUserInFirestore()
-    fun getUserInFirestore(): FirebaseUser?
-
-    suspend fun getOpenQueue()
-    suspend fun joinQueue()
-    suspend fun detailQueue()
-    suspend fun exitQueue()
-
+    suspend fun getOpenQueue() : Result<List<Queue>?>
+    suspend fun joinQueue(idQueue : String, idMember : String) : Result<Void?>
 }
